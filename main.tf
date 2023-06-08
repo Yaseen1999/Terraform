d623a9dd52d3bb661db74e30396d3c358f5f658f
Yaseen1999 <130662667+Yaseen1999@users.noreply.github.com>
2023-06-08 04:43:49 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider  "aws" {
 region = "ap-south-1"
-access_key = "AKIA2MBNIDBRR3BZEUYL"
-secret_key = "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"
+access_key = "AKIA2MBNIDBRWSF7F6N4"
+secret_key = "cqvZWTblTNAM4lllEdqVou4uBjABENM5fdmiqrPu"
 }
 
 resource "aws_instance" "example" {
@@ -9,6 +9,6 @@ ami     = "ami-0f5ee92e2d63afc18"
 instance_type = "t2.micro"
 key_name      = "terra"
 tags          = {
-Name          = "vcube"
+Name          = "from terra script"
 }
 }
